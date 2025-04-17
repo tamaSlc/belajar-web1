c3977478ed0843a2cebb0b5a135ac65e6759d6ab
tamaSlc <reeveell122@gmail.com>
2025-04-17 21:59:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function tampilkanPesan() {
+  alert("Terima kasih sudah mengklik!");
+}
+
+function kirimForm(event) {
+  event.preventDefault();
+  alert("Form berhasil dikirim!");
+  return false;
+}
